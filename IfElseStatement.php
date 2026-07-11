3a9db270194e777950c9b40928a34af9cd34b417
habibtalha0-cloud <habibtalha0@gmail.com>
2026-07-11 19:35:54 +0500

    Add files via upload

---
+++ IfElseStatement.php
@@ -0,0 +1,30 @@
+<?php
+
+// $age = 15;
+
+// if($age>= 18){
+//     echo "You are eligible for voting";
+// }
+// else{
+//     echo "You are not eligible for voting";
+// }
+
+$percentage = 70;
+if($percentage>=90){
+    echo "You got A1";
+}
+ else if($percentage>=80){
+    echo "You got A+";
+ }
+  else if($percentage>=70){
+    echo "You got A";
+ }
+  else if($percentage>=70){
+    echo "You got B";
+ }
+  else if($percentage>=60){
+    echo "You got C";
+ }
+  else {
+    echo "You are fail";
+ }
---
+++ variables.php
@@ -0,0 +1,14 @@
+<?php
+
+$username = "Talha";
+$num = 25;
+$bool = True;
+$float = 34.3;
+
+define('pi', 3.14);
+echo pi ;
+
+echo "$username <br>";
+echo "$num <br>";
+echo "$bool <br>";
+echo "$float <br>";
